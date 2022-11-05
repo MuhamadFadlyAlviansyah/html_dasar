3c94680a09e1691b7949da8980b9a9e80404eb19
fadlyalvi <muhamadfadlyalviansyah@smkwikrama.sch.id>
2022-11-05 12:24:02 +0700

    Personal web

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var navlist = document.querySelector(".nav-list");
 var links = document.querySelector(".nav-list li");
 
 hamburger.addEventListener("click", function(){
-    this.classList.toogle("click");
-    navlist.classList.toogle("open");
+    this.classList.toggle("click");
+    navlist.classList.toggle("open");
 });
